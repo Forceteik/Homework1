d73e2e25fe4d61da60854d1dd7d46448334cabb7
Forceteik <80638590+Forceteik@users.noreply.github.com>
2021-09-19 01:12:50 +0600

    Add files via upload

---
+++ src/head.py
@@ -0,0 +1,15 @@
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+from itertools import islice
+
+print ("Plese, write your N top:")
+num = int(input()) #user input amount of coins from list
+
+coins = cg.get_coins_markets(vs_currency="usd") # this is a list
+
+sorted_coins = sorted(coins, key=lambda coin: coin['current_price']) # sorted can accept custom comparator. sort py price
+
+res = list(islice(reversed(sorted_coins), 0, num)) #reverse list for output
+res.reverse()
+
+print(res) 
